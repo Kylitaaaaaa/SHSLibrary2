4423f8e90b91945616f115efb47bbb987ed56efa
Kylitaaaaaa <kyle-althea_santos@dlsu.edu.ph>
2017-07-05 01:05:55 +0800

    With Add Admin

--- src/Controller.java
+++ src/Controller.java
@@ -63,6 +63,29 @@ public class Controller extends HttpServlet {
 			break;
 		}
 	}
+	
+	protected void addAdministrator(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		//change string to int
+		String idNum =  request.getParameter("id_num");
+		String fName = request.getParameter("first_name");
+		String lName = request.getParameter("m_initial");
+		String mi = request.getParameter("last_name");
+		String mNum = request.getParameter("mobile_number");
+		String email = request.getParameter("email_address");
+		String sQuestion = request.getParameter("secret_question");
+		String sAnswer = request.getParameter("secret_answer");
+		String birthday = request.getParameter("birthday");
+		String password = request.getParameter("password");
+		
+		
+		if(idNum != null & fName != null & lName != null & mi != null & mNum  != null & email != null & sQuestion != null & sAnswer != null & birthday != "" & password != null){
+			int adminId = UserService.addAdminUser(idNum, password, email, mNum, "3");
+			adminService.addLibraryManager(Integer.toString(adminId),fName, lName, mi, sQuestion, sAnswer, birthday);
+
+		}			
+		else 
+			System.out.println("Aww");				
+	}
 
 	protected void addManager(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//change string to int
