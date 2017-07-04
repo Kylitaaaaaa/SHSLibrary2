65235456db62633211552c1e88b09c4567ac9d44
Kylitaaaaaa <kyle-althea_santos@dlsu.edu.ph>
2017-07-05 00:49:28 +0800

    Add Customer, Manager and Staff

--- src/Controller.java
+++ src/Controller.java
@@ -79,7 +79,7 @@ public class Controller extends HttpServlet {
 		
 		
 		if(idNum != null & fName != null & lName != null & mi != null & mNum  != null & email != null & sQuestion != null & sAnswer != null & birthday != "" & password != null){
-			int adminId= UserService.addAdminUser(idNum, password, email, mNum);
+			int adminId= UserService.addAdminUser(idNum, password, email, mNum, "0");
 			adminService.addLibraryManager(Integer.toString(adminId),fName, lName, mi, sQuestion, sAnswer, birthday);
 
 		}
@@ -103,37 +103,11 @@ public class Controller extends HttpServlet {
 		
 		
 		if(idNum != null & fName != null & lName != null & mi != null & mNum  != null & email != null & sQuestion != null & sAnswer != null & birthday != "" & password != null){
-			UserService.addAdminUser(idNum, password, email, mNum);
-			int adminId= UserService.addAdminUser(idNum, password, email, mNum);
+			int adminId= UserService.addAdminUser(idNum, password, email, mNum, "1");
 			adminService.addLibraryStaff(Integer.toString(adminId),fName, lName, mi, sQuestion, sAnswer, birthday);
 
 		}
 			
-		else 
-			System.out.println("Aww");				
-	}
-	
-
-	protected void addAdministrator(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		//change string to int
-		String idNum =  request.getParameter("id_num");
-		String fName = request.getParameter("first_name");
-		String lName = request.getParameter("m_initial");
-		String mi = request.getParameter("last_name");
-		String mNum = request.getParameter("mobile_number");
-		String email = request.getParameter("email_address");
-		String sQuestion = request.getParameter("secret_question");
-		String sAnswer = request.getParameter("secret_answer");
-		String birthday = request.getParameter("birthday");
-		String password = request.getParameter("password");
-		
-		
-		if(idNum != null & fName != null & lName != null & mi != null & mNum  != null & email != null & sQuestion != null & sAnswer != null & birthday != "" & password != null){
-			UserService.addAdminUser(idNum, password, email, mNum);
-			int adminId= UserService.addAdminUser(idNum, password, email, mNum);
-			adminService.addAdministrator(Integer.toString(adminId),fName, lName, mi, sQuestion, sAnswer, birthday);
-
-		}			
 		else 
 			System.out.println("Aww");				
 	}
@@ -154,9 +128,8 @@ public class Controller extends HttpServlet {
 		
 		
 		if(idNum != null & fName != null & lName != null & mi != null & mNum  != null & email != null & sQuestion != null & sAnswer != null & birthday != "" & password != null){
-			UserService.addCustomerUser(idNum, password, email, mNum);
-			String customerId= ""/*Get UserID where COLUMN_ID_NUMBER = idNum */;
-			customerService.addProfessor(customerId,fName, lName, mi, sQuestion, sAnswer, birthday);
+			int customerId= UserService.addCustomerUser(idNum, password, email, mNum, "0");
+			customerService.addProfessor(Integer.toString(customerId),fName, lName, mi, sQuestion, sAnswer, birthday);
 		}			
 		else 
 			System.out.println("Aww");				
@@ -176,9 +149,8 @@ public class Controller extends HttpServlet {
 		
 		
 		if(idNum != null & fName != null & lName != null & mi != null & mNum  != null & email != null & sQuestion != null & sAnswer != null & birthday != "" & password != null){
-			UserService.addCustomerUser(idNum, password, email, mNum);
-			String customerId= ""/*Get UserID where COLUMN_ID_NUMBER = idNum */;
-			customerService.addStudent(customerId,fName, lName, mi, sQuestion, sAnswer, birthday);
+			int customerId= UserService.addCustomerUser(idNum, password, email, mNum, "1");
+			customerService.addProfessor(Integer.toString(customerId),fName, lName, mi, sQuestion, sAnswer, birthday);
 		}			
 		else 
 			System.out.println("Aww");				
--- src/UserService.java
+++ src/UserService.java
@@ -7,42 +7,7 @@ import java.sql.Statement;
 public class UserService {
 	public UserService(){}
 
-	public static void addCustomerUser(String idNum, String password, String email, String mNumber){
-		
-		
-		String sql = "INSERT INTO " + User.TABLE_NAME + " (" +
-				User.COLUMN_ID_NUMBER + ", " +
-				User.COLUMN_PASSWORD + ", " +
-				 User.COLUMN_USER_TYPE + ", " + 
-				 User.COLUMN_EMAIL + ", " +
-				 User.COLUMN_PHONE_NUMBER + ") " +
-				 " VALUES (?, ?, ?, ?, ?)";
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = DBPool.getInstance().getConnection();
-			pstmt=conn.prepareStatement(sql);
-			pstmt.setInt(1, Integer.parseInt(idNum));
-			pstmt.setString(2, password);
-			pstmt.setString(3, "1");
-			pstmt.setString(4, email);
-			pstmt.setString(5, mNumber);
-			
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally{
-			try {
-				pstmt.close();
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	public static int addAdminUser(String idNum, String password, String email, String mNumber){
+	public static int addCustomerUser(String idNum, String password, String email, String mNumber, String customerType){
 		
 		
 		String sql = "INSERT INTO " + User.TABLE_NAME + " (" +
@@ -62,7 +27,51 @@ public class UserService {
 			pstmt=conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 			pstmt.setInt(1, Integer.parseInt(idNum));
 			pstmt.setString(2, password);
-			pstmt.setString(3, "0");
+			pstmt.setString(3, customerType);
+			pstmt.setString(4, email);
+			pstmt.setString(5, mNumber);
+			
+			pstmt.executeUpdate();
+			
+			ResultSet rs = pstmt.getGeneratedKeys();
+
+            if (rs.next()) {
+            	auto_id = rs.getInt(1);
+            }
+            
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return auto_id;
+	}
+	public static int addAdminUser(String idNum, String password, String email, String mNumber, String userType){
+		
+		
+		String sql = "INSERT INTO " + User.TABLE_NAME + " (" +
+				User.COLUMN_ID_NUMBER + ", " +
+				User.COLUMN_PASSWORD + ", " +
+				 User.COLUMN_USER_TYPE + ", " + 
+				 User.COLUMN_EMAIL + ", " +
+				 User.COLUMN_PHONE_NUMBER + ") " +
+				 " VALUES (?, ?, ?, ?, ?)";
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int auto_id = -1;
+		
+		try {
+			conn = DBPool.getInstance().getConnection();
+			pstmt=conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			pstmt.setInt(1, Integer.parseInt(idNum));
+			pstmt.setString(2, password);
+			pstmt.setString(3, userType);
 			pstmt.setString(4, email);
 			pstmt.setString(5, mNumber);
 			
