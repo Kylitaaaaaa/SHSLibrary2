d89639dd5da9fe678d388b006e476b969b6ef496
Kylitaaaaaa <kyle-althea_santos@dlsu.edu.ph>
2017-07-06 00:57:04 +0800

    With get all meeting rooms

--- src/RoomService.java
+++ src/RoomService.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -12,11 +13,87 @@ public class RoomService {
 	 */
 	
 	public static ArrayList<Meeting_Room> getAllMeetings(){
+		ArrayList<Meeting_Room> mList = new ArrayList<Meeting_Room>();
+		String sql = "SELECT * FROM meeting_room";
 		
-		//String sql = String.format("SELECT *");
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBPool.getInstance().getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				
+				Meeting_Room m = new Meeting_Room();
+				m.setMeetingRoomId(rs.getInt(Meeting_Room.COLUMN_ROOM_ID));
+				m.setRoomName(rs.getString(Meeting_Room.COLUMN_ROOM_NAME));
+				m.setRoomStatus(rs.getInt(Meeting_Room.COLUMN_ROOM_STATUS));
+				
+				mList.add(m);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return mList;
 		
-		return null;
+	}
+	
+	public static ArrayList<Admin> getAllUserLibraryManager(){
+		ArrayList<Admin> adminList = new ArrayList<Admin>();
 		
+		//String sql="SELECT * FROM " + Admin.TABLE_NAME + ";";
+//		SELECT * FROM shslibrary.user u,admin a
+//		where u.userID=a.adminId;
+		
+		String sql = "SELECT * FROM User u, Admin a WHERE u.userID = a.adminId";
+		
+		//String sql = "SELECT * FROM " + User.TABLE_NAME + " u, " + Admin.TABLE_NAME + " a WHERE u." + User.COLUMN_ID_NUMBER + " = ?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBPool.getInstance().getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				
+				
+				Admin a = new Admin();
+				a.setIdNumber(rs.getInt(User.COLUMN_ID_NUMBER));
+				a.setPassword(rs.getString(User.COLUMN_PASSWORD));
+				a.setUserType(rs.getInt(User.COLUMN_USER_TYPE));
+				a.setEmailAddress(rs.getString(User.COLUMN_EMAIL));
+				a.setmNumber(rs.getString(User.COLUMN_PHONE_NUMBER));
+				a.setLockStatus(rs.getInt(User.COLUMN_LOCK_STATUS));
+				a.setLoginAttempts(rs.getInt(User.COLUMN_LOGIN_ATTEMPTS));
+				
+				a.setFirstName(rs.getString(Admin.COLUMN_ADMIN_FIRST_NAME));
+				a.setLastName(rs.getString(Admin.COLUMN_ADMIN_LAST_NAME));
+				a.setMiddleInitial(rs.getString(Admin.COLUMN_ADMIN_MIDDLE_INITIAL));
+				a.setBirthday(rs.getString(Admin.COLUMN_ADMIN_BIRTHDAY));
+				adminList.add(a);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return adminList;
 	}
 	
 	public static void reserveRoom(String roomId, String startTime, String endTime, String reservationDate, String dateReserved, String status){
