3120c822c710b757574ac33593a86ef40c5c3274
Benson Polican <benson_polican@dlsu.edu.ph>
2017-07-05 23:15:14 +0800

    Increase Login Attempt

--- src/UserService.java
+++ src/UserService.java
@@ -135,4 +135,93 @@ public class UserService {
 		
 		return auto_id;
 	}
+	
+	public static void increaseLoginCount(String idNumber){
+		String sql = "SELECT * FROM "+User.TABLE_NAME+" WHERE "+User.COLUMN_ID_NUMBER+"="+idNumber+";";
+		
+		User u = new User();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBPool.getInstance().getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()){				
+				u.setLockStatus(rs.getInt(User.COLUMN_LOCK_STATUS));
+				u.setLoginAttempts(rs.getInt(User.COLUMN_LOGIN_ATTEMPTS));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(u.getLockStatus()==0&&u.getLoginAttempts()<3){
+			u.setLoginAttempts(u.getLoginAttempts()+1);
+			if(u.getLoginAttempts()==3)
+			{
+				String sql2 = "UPDATE " + User.TABLE_NAME + " SET " + 
+						 User.COLUMN_LOCK_STATUS + "= ?, " +
+						 User.COLUMN_LOGIN_ATTEMPTS + "= ?, " +
+						 " WHERE " + User.COLUMN_ID_NUMBER + "= ?";
+						 
+				Connection connection = DBPool.getInstance().getConnection();
+				PreparedStatement pstmt2 = null;
+				
+				try {
+					pstmt2 = connection.prepareStatement(sql2);
+					pstmt2.setString(1, "1");
+					pstmt2.setString(2, "0");
+					pstmt2.setString(3, idNumber);
+					
+					
+					pstmt.executeUpdate();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						pstmt2.close();
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			else{
+				String sql2 = "UPDATE " + User.TABLE_NAME + " SET " + 
+						 User.COLUMN_LOGIN_ATTEMPTS + "= ?, " +
+						 " WHERE " + User.COLUMN_ID_NUMBER + "= ?";
+						 
+				Connection connection = DBPool.getInstance().getConnection();
+				PreparedStatement pstmt2 = null;
+				
+				try {
+					pstmt2 = connection.prepareStatement(sql2);
+					pstmt2.setString(1, u.getLoginAttempts()+"");
+					pstmt2.setString(2, idNumber);
+					
+					
+					pstmt.executeUpdate();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						pstmt2.close();
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+			
+	}
 }
